b5c94e139080cc45f4b15d280cc2280ed98ce329
noamma <android@nvgnet.co.il>
2017-11-01 16:09:02 +0200

    Update setAngularEnv.sh

--- setAngularEnv.sh
+++ setAngularEnv.sh
@@ -20,9 +20,9 @@ sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo npm install -g @angular/cli@latest
-cd
+cd ~
 mkdir Git
-mkdir  ./Git/AngularJS
+mkdir  ~/Git/AngularJS
 mkdir $f
 cd ~/Git/AngularJS/$f
 sudo ng new $f
