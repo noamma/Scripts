f2e871fe038a7a52d29c7b81bc86af48bb4083bc
noamma <android@nvgnet.co.il>
2017-11-06 17:28:36 +0200

    Create getXamp.sh

---
+++ getXamp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://www.apachefriends.org/xampp-files/5.6.20/xampp-linux-x64-5.6.20-0-installer.run
+sudo chmod +x xampp-linux-x64-5.6.20-0-installer.run
+sudo ./xampp-linux-x64-5.6.20-0-installer.run
