de2c19c7c586c5b1d2c021fedca8e6437c5efe3a
noamma <android@nvgnet.co.il>
2017-11-22 00:01:39 +0200

    fixed eclipse installer

--- getEclipse.sh
+++ getEclipse.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-wget https://www.eclipse.org/downloads/download.php?file=/oomph/epp/oxygen/R/eclipse-inst-linux64.tar.gz
+cd ~/Downloads
+wget http://mirrors.uniri.hr/eclipse//oomph/epp/oxygen/R/eclipse-inst-linux64.tar.gz
+sudo tar -xvzf eclipse-inst-linux64.tar.gz -C /opt
+/opt/eclipse-installer/eclipse-inst
