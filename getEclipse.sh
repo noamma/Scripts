9d8d749a6b7609d212d5487ee70b6ffb92f5ac2c
noamma <android@nvgnet.co.il>
2017-11-22 00:49:21 +0200

    Update getEclipse.sh

--- getEclipse.sh
+++ getEclipse.sh
@@ -3,3 +3,4 @@ cd ~/Downloads
 wget http://mirrors.uniri.hr/eclipse//oomph/epp/oxygen/R/eclipse-inst-linux64.tar.gz
 sudo tar -xvzf eclipse-inst-linux64.tar.gz -C /opt
 /opt/eclipse-installer/eclipse-inst
+sudo gedit /usr/share/applications/eclipse.desktop
